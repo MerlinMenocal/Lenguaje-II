e5d679d6bb48730a78288ab75a30f76cd2b901e2
Merlin Menocal <merlinmm0530@gmail.com>
2022-06-07 22:27:43 -0600

    cambio 5

--- L2/Ejercicio 01 Biblioteca/biblioteca.cpp
+++ L2/Ejercicio 01 Biblioteca/biblioteca.cpp
@@ -82,8 +82,8 @@ int main(int argc, char const *argv[])
                 cout<<"Libro encontrado: "<<libro <<endl;
 
                 //autores
-                int autor1 = rand()% 38 + 1;
-                cout<<"--> Autor: "<<libros[autor1][2]<<endl;
+                int autorPrincipal = rand()% 38 + 1;
+                cout<<"--> Autor: "<<libros[autorPrincipal][2]<<endl;
 
                 cout<<endl<<endl;
 
@@ -91,8 +91,12 @@ int main(int argc, char const *argv[])
                 int sugerencia2 = rand()% 38 + 1;
                 int sugerencia3 = rand()% 38 + 1;
 
+                int autorSugerencia1 = rand()% 38 + 1;
+                int autorSugerencia2 = rand()% 38 + 1;
+                int autorSugerencia3 = rand()% 38 + 1;
+
                 cout<<"  Otras Sugerencias..."<<endl;
-                cout<<"Sugerencia 1: "<< libros[sugerencia1][1]<<" --> "<<libros[autor1][2]<<endl;
+                cout<<"Sugerencia 1: "<< libros[sugerencia1][1]<<" --> "<<libros[autorSugerencia3][2]<<endl;
                 cout<<"Sugerencia 2: "<< libros[sugerencia2][1]<<endl;
                 cout<<"Sugerencia 3: "<< libros[sugerencia3][1]<<endl;
 
