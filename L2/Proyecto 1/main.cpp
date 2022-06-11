778cd75ec2d7c5230b5ce7f17c6f9205f1948542
Merlin Menocal <merlinmm0530@gmail.com>
2022-06-11 16:20:53 -0600

    cambio 1.3

--- L2/Proyecto 1/main.cpp
+++ L2/Proyecto 1/main.cpp
@@ -34,7 +34,7 @@ int main(int argc, char const *argv[])
         default:
             break;
         }
-        system("pause");
+        system("cls");
         cout<<endl;
 
         if (opcion == 0)
